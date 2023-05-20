93f6562607c5d20966730eaebbb0a3a67d58c45c
Kay <Kagyemang201@gmail.com>
2023-05-20 15:05:42 -0700

    Javascript  1 Ex 2

---
+++ myscript.js
@@ -0,0 +1,59 @@
+//ARITHMETIC OPERATORS
+
+//ADDITION
+console.log(25 + 12);
+console.log(32 + 12);
+console.log(12 + 1);
+
+//SUBTRACTION
+console.log(51 - 15);
+console.log(13 - 8);
+console.log(43 - 3);
+
+//MULTIPLICATION
+console.log(12 * 4);
+console.log(5 * 5);
+console.log(2 * 3);
+
+
+//COMPARISON
+//EQUAL TO
+console.log(17 == 71);
+console.log(4 == "13");
+ console.log(17 == "00");
+
+// IDENTICAL
+ console.log("12" === 13.2);
+ console.log(7 === 7);
+ console.log(10 === 10);
+
+// NOT EQUAL TO
+console.log(42 != 42);
+console.log(14 != 7);
+console.log("Kay" != "Kya");
+
+
+//LOGICAL AND CONDITIONAL
+let num1 = 21;
+let num2 = 17; 
+let num3 = 25;
+let num4 = 40;
+ 
+ //LOGICAL AND
+ console.log(num1 > num2 && num3 < num4);
+ 
+//LOGICAL OR
+ console.log(num1 > num3 || num2 < num4);
+ 
+
+//LOGICAL NOT
+ console.log(!false);
+
+// //concatenation
+
+ let name = "KwabS";
+ let num = 9;
+
+ console.log("My name is " + name);
+ console.log("My name is " + num);
+ console.log("My name is " + "Agyemang");
